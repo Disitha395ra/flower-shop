2fa839b8acbee3c6690db7d1f8280b4c6f94b587
Disitha <disithar@gmail.com>
2025-01-07 01:10:15 +0530

    enhance Home component with welcome text and buttons for improved user interaction

--- flowerapp/components/Home.js
+++ flowerapp/components/Home.js
@@ -1,13 +1,31 @@
 import {StyleSheet,View,ScrollView,Image} from 'react-native';
-import {PaperProvider} from 'react-native-paper';
+import { PaperProvider, Text,Button } from "react-native-paper";
 export default function Home(){
     return (
       <PaperProvider>
         <ScrollView>
           <View>
             <Image
-                source={require('../assets/Home.jpg')}
-            style={styles.homebanner}/>
+              source={require("../assets/Home.jpg")}
+              style={styles.homebanner}
+            />
+            <Text style={styles.hometext}>Welcome To Flowers</Text>
+            <Button
+              icon="camera"
+              mode="contained"
+              onPress={() => console.log("Pressed")}
+              style={styles.homebutton}
+            >
+              Press me
+            </Button>
+            <Button
+              icon="camera"
+              mode="contained"
+              onPress={() => console.log("Pressed")}
+              style={styles.homebutton}
+            >
+              Press me
+            </Button>
           </View>
         </ScrollView>
       </PaperProvider>
@@ -15,8 +33,24 @@ export default function Home(){
 }
 
 const styles = StyleSheet.create({
-  homebanner:{
-    width:"100%",
-    height:900,
+  homebanner: {
+    width: "100%",
+    height: 900,
+  },
+  hometext: {
+    fontSize: 30,
+    color: "white",
+    position: "absolute",
+    top: 300,
+    marginLeft: 75,
+    textAlign: "center",
+  },
+  homebutton:{
+    position: "absolute",
+    top: 400,
+    marginLeft: 65,
+    width: 300,
+    height: 50,
+    
   }
 });
