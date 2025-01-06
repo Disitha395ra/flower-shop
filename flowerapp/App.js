850c21ff77050b715b4c488d47890f01d3c90da9
Disitha <disithar@gmail.com>
2025-01-07 00:58:04 +0530

    refactor App and Home components to use ScrollView and PaperProvider for improved layout and add styling to the home banner image

--- flowerapp/App.js
+++ flowerapp/App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,ScrollView } from 'react-native';
+import {PaperProvider} from 'react-native-paper';
+import Home from './components/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <ScrollView>
+        <View>
+          <Home/>
+        </View>
+      </ScrollView>
+    </PaperProvider>
   );
 }
 
--- flowerapp/components/Home.js
+++ flowerapp/components/Home.js
@@ -1,19 +1,22 @@
 import {StyleSheet,View,ScrollView,Image} from 'react-native';
-import {Paperprovider} from 'react-native-paper';
+import {PaperProvider} from 'react-native-paper';
 export default function Home(){
     return (
-      <Paperprovider>
+      <PaperProvider>
         <ScrollView>
           <View>
             <Image
                 source={require('../assets/Home.jpg')}
-            />
+            style={styles.homebanner}/>
           </View>
         </ScrollView>
-      </Paperprovider>
+      </PaperProvider>
     );
 }
 
 const styles = StyleSheet.create({
-
-})
+  homebanner:{
+    width:"100%",
+    height:900,
+  }
+});
