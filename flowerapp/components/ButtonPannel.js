575546608f6685961a8790242ca97b65a744666e
Disitha <disithar@gmail.com>
2025-01-07 09:52:31 +0530

    add route parameters to ButtonPannel component and integrate bottom tab navigation

--- flowerapp/components/ButtonPannel.js
+++ flowerapp/components/ButtonPannel.js
@@ -1,11 +1,17 @@
 import {StyleSheet,View} from 'react-native';
 import {PaperProvider,Text} from 'react-native-paper';
-
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Item from './Item';
 import Cart from './Cart';
 import Profile from './Profile';
 
-export default function ButtonPannel(){
+const Tab = createBottomTabNavigator();
+
+export default function ButtonPannel({route}){
+
+    const {username,password} = route.params;
+
     return(
         <View>
             <Text>ButtonPannel</Text>
