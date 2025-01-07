393894e3b3e6b5d26323ecc99182736b3b5fe9f8
Disitha <disithar@gmail.com>
2025-01-07 13:41:30 +0530

    feat: enhance Item component with email input and icon buttons for improved interactivity

--- flowerapp/components/Item.js
+++ flowerapp/components/Item.js
@@ -6,30 +6,55 @@ import {
   TextInput,
   Avatar,
   Card,
+  IconButton,
+  MD3Colors,
 } from "react-native-paper";
 import {flowers} from '../data/FlowerDB'
+
+import React from "react";
+
 export default function Item(){
+const [text, setText] = React.useState("");
     return (
       <PaperProvider>
         <ScrollView>
-            {flowers.map((flower, id) => (
-               <Card key={flower.id} style={styles.cardcomponent}>
-                 <Card.Content>
-                   <Text variant="titleLarge">Name - {flower.name}</Text>
-                   <Text variant="bodyMedium">Price - {flower.price}</Text>
-                 </Card.Content>
-                 <Card.Cover source={flower.img}  style={styles.cardphoto}/>
-                 <Card.Actions>
-                   <Button
-                     icon="cart-plus"
-                     mode="contained"
-                     onPress={() => console.log("Pressed")}
-                   >
-                     Add to Cart
-                   </Button>
-                 </Card.Actions>
-               </Card>
-            ))}
+          {flowers.map((flower, id) => (
+            <Card key={flower.id} style={styles.cardcomponent}>
+              <Card.Content>
+                <Text variant="titleLarge">Name - {flower.name}</Text>
+                <Text variant="bodyMedium">Price - {flower.price}</Text>
+              </Card.Content>
+              <Card.Cover source={flower.img} style={styles.cardphoto} />
+              <Card.Actions>
+                <IconButton
+                  icon="plus"
+                  iconColor={MD3Colors.error50}
+                  size={20}
+                  onPress={() => console.log("Pressed")}
+                />
+                <TextInput
+                  label="Email"
+                  value={text}
+                  onChangeText={(text) => setText(text)}
+                  editable={false}
+                  style={styles.textfield}
+                />
+                <IconButton
+                  icon="minus"
+                  iconColor={MD3Colors.error50}
+                  size={20}
+                  onPress={() => console.log("Pressed")}
+                />
+                <Button
+                  icon="cart-plus"
+                  mode="contained"
+                  onPress={() => console.log("Pressed")}
+                >
+                  Add to Cart
+                </Button>
+              </Card.Actions>
+            </Card>
+          ))}
         </ScrollView>
       </PaperProvider>
     );
@@ -40,8 +65,17 @@ const styles = StyleSheet.create({
     margin: 10,
     padding: 10,
   },
-  cardphoto:{
-    position: 'relative',
+  cardphoto: {
+    position: "relative",
     height: 500,
-  }
+  },
+  textfield: {
+    width: 100,
+    alignContent: "center",
+    textAlign: "center",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
 });
