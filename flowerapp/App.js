a8e976952ec76cca0c2192e4b2e61bcbc0d2622d
Disitha <disithar@gmail.com>
2025-01-07 09:25:28 +0530

    implement navigation stack for Home, Login, and Signup components

--- flowerapp/App.js
+++ flowerapp/App.js
@@ -1,17 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,ScrollView } from 'react-native';
 import {PaperProvider} from 'react-native-paper';
-//import Home from './components/Home';
-//import Login from './components/Login';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from './components/Home';
+import Login from './components/Login';
 import Signup from './components/Signup';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <PaperProvider>
       <ScrollView>
-        <View>
-          <Signup/>
-        </View>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={Home}/>
+            <Stack.Screen name="Login" component={Login}/>
+            <Stack.Screen name="Signup" component={Signup}/>
+          </Stack.Navigator>
+        </NavigationContainer>
       </ScrollView>
     </PaperProvider>
   );
