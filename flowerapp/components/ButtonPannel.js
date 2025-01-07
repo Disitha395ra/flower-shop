26aee02028d23b045be27374cd6d6e92f229a334
Disitha <disithar@gmail.com>
2025-01-07 09:42:15 +0530

    add ButtonPannel component with basic structure

---
+++ flowerapp/components/ButtonPannel.js
@@ -0,0 +1,13 @@
+import {StyleSheet,View} from 'react-native';
+import {PaperProvider,Text} from 'react-native-paper';
+export default function ButtonPannel(){
+    return(
+        <View>
+            <Text>ButtonPannel</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
