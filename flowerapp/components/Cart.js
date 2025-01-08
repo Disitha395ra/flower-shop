5e4b3584bc8bb22a8990d03a6b8ce363facd749d
Disitha <disithar@gmail.com>
2025-01-08 10:41:38 +0530

    fix: enhance Cart component to display flower details and handle default route parameters

--- flowerapp/components/Cart.js
+++ flowerapp/components/Cart.js
@@ -1,11 +1,20 @@
-import { StyleSheet, ScrollView } from "react-native";
+import { StyleSheet, ScrollView,View } from "react-native";
 import { PaperProvider, Text, Button, TextInput } from "react-native-paper";
+import { flowers } from "../data/FlowerDB";
 export default function Cart({route}) {
-  const {id, counts} = route.params;
+  const { id = "Unknown", count = 0 } = route.params || {};
+  const flowerdetails = flowers.find(flower=>flower.id ===id);
   return (
     <PaperProvider>
-      <ScrollView>
-        <Text>Hiii</Text>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.card}>
+          <Text variant="titleLarge" style={styles.text}>
+            Flower Name : {flowerdetails.name}
+          </Text>
+          <Text variant="bodyLarge" style={styles.text}>
+            Quantity: {count}
+          </Text>
+        </View>
       </ScrollView>
     </PaperProvider>
   );
